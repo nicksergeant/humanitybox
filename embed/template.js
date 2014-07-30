85a250ce5fd7bda3b9f08efe2ca7386a4bc6c3c8
Nick Sergeant <nick@nicksergeant.com>
2014-07-29 23:37:21 -0400

    Work on embed JS and CSS.

--- embed/template.js
+++ embed/template.js
@@ -3,10 +3,13 @@
   var randomIndex = Math.floor((Math.random() * campaigns.length) + 0);
   var campaign = campaigns[randomIndex];
   var html = "<style><%- css %></style>";
-  html += '<div class="humanitybox">';
-  html += '  <h6>' + campaign.title + '</h6>';
+  html += '<a class="humanitybox" href="' + campaign.url + '">';
+  html += '  <span class="humanitybox-title">' + campaign.title + '</span>';
   html += '  <img src="' + campaign.image_url + '" />';
-  html += '  <p>' + campaign.blurb + '</p>';
-  html += '</div>';
+  html += '  <span class="humanitybox-blurb">' + campaign.blurb + '</span>';
+  html += '</a>';
+  html += '<a class="humanitybox-link" href="http://humanitybox.com/">';
+  html += '  <img src="http://humanitybox.com/favicon.ico" /> Humanity Box'
+  html += '</a>';
   document.write(html);
 }(document));
