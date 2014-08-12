6c7236dc31debb7c76026ac4962629ee32555339
Nick Sergeant <nick@nicksergeant.com>
2014-08-12 00:22:17 -0400

    URL exclusions.

--- scripts/make/stats.js
+++ scripts/make/stats.js
@@ -88,7 +88,7 @@ sequence.then(function(next) {
   logs.forEach(function(log) {
     var url = log.split('\t')[9];
     var domainParts = url.match('^(?:(?:f|ht)tps?://)?([^/:]+)');
-    if (domainParts.length > 1) {
+    if (domainParts.length > 1 && url !== '-' && url !== '0.0.0.0' && url !== 'localhost') {
       url = domainParts[1];
       logLinesSequence.then(function(logLinesSequenceNext) {
         r.table('stats').filter({ url: url })
