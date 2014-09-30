1ef2227ea939c815bf394f3c3eb9b29c7ddaa67c
Nick Sergeant <nick@nicksergeant.com>
2014-09-30 11:45:35 -0400

    Open links in new tab.

--- embed/template.js
+++ embed/template.js
@@ -3,12 +3,12 @@
   var randomIndex = Math.floor((Math.random() * campaigns.length) + 0);
   var campaign = campaigns[randomIndex];
   var html = "<style><%- css %></style>";
-  html += '<a class="humanitybox" href="' + campaign.url + '">';
+  html += '<a class="humanitybox" href="' + campaign.url + '" target="_blank">';
   html += '  <span class="humanitybox-title">' + campaign.title + '</span>';
   html += '  <span class="humanitybox-img-container"><img src="' + campaign.image_url + '" /></span>';
   html += '  <span class="humanitybox-blurb">' + campaign.blurb + '</span>';
   html += '</a>';
-  html += '<a class="humanitybox-link" href="http://humanitybox.com/">';
+  html += '<a class="humanitybox-link" href="http://humanitybox.com/" target="_blank">';
   html += '  <img src="http://humanitybox.com/favicon.ico" /> <span>Humanity Box</span>';
   html += '</a>';
   var div = document.getElementById('humanitybox');
