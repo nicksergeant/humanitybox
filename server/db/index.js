b0da9edfd2da93f166ea1259c76b3bb69096552a
Nick Sergeant <nick@nicksergeant.com>
2016-12-06 22:09:12 -0500

    Switch mongo env.

--- server/db/index.js
+++ server/db/index.js
@@ -2,7 +2,7 @@ var MongoClient = require('mongodb').MongoClient;
 var Promise = require('bluebird');
 
 module.exports = new Promise(function(resolve, reject) {
-  MongoClient.connect(process.env.MONGO_URL || 'mongodb://localhost/humanitybox', function(err, conn) {
+  MongoClient.connect(process.env.MONGODB_URI || 'mongodb://localhost/humanitybox', function(err, conn) {
     if (err) throw err;
     resolve(conn);
   });
